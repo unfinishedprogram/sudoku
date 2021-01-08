b58a74d1941a027731c72fca778739941cd50f11
UnfinishedProgram <noah.labrecque@gmail.com>
2021-01-08 12:38:14 -0500

    Reformatted display to seperate it form logic

--- sudoku.py
+++ sudoku.py
@@ -4,49 +4,110 @@ import pygame
 import pygame.key
 
 
+
+
 class Sudoku(object):
     def __init__(self):
-        # initalize UI
-        pygame.init()
-
-        size = width, height = 320, 240
-        speed = [2, 2]
-        black = 0, 0, 0
-
-        screen = pygame.display.set_mode(size)
-
-        ball = pygame.image.load("intro_ball.gif")
-        ballrect = ball.get_rect()
-
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
-
-            ballrect = ballrect.move(speed)
-            if ballrect.left < 0 or ballrect.right > width:
-                speed[0] = -speed[0]
-            if ballrect.top < 0 or ballrect.bottom > height:
-                speed[1] = -speed[1]
-
-            screen.fill(black)
-            screen.blit(ball, ballrect)
-            pygame.display.flip()
-
+        self.ui = UI(self)
+        self.screen = None
         self.regions = [[]] * 9
         self.cells = []
+        self.selectedCell = None
+
         for i in range(9):
             for j in range(9):
                 self.cells.append(Cell(self, math.floor(i / 3) * 3 + math.floor(j / 3), i, j))
                 self.regions[math.floor(i / 3) * 3 + math.floor(j / 3)].append([self.cells[len(self.cells) - 1]])
+        self.gameloop()
 
-    def print(self):
-        index = 0
-        for cell in self.cells:
-            if (index > 8):
-                index = 0
-                print()
-            print(cell.number, end='')
-            index += 1
+    def get_object_at(self, mouse):
+        if(mouse[1] < self.screenSize[0]):
+            square_x = math.floor(mouse[0] / (self.screenSize[0] / 9))
+            square_y = math.floor(mouse[1] / (self.screenSize[0] / 9))
+            print(square_x, square_y)
+            return self.cells[square_y * 9 + square_x]
+        else:
+            pass
+
+    def select_cell(self, cell):
+        if(cell):
+            if(self.selectedCell):
+                self.selectedCell.deselect()
+            self.selectedCell = cell
+            cell.select()
+
+    def set_number(self, number):
+        if self.selectedCell:
+            self.selectedCell.set_number(number)
+
+    def gameloop(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print("MouseDown")
+                if event.type == pygame.MOUSEBUTTONUP:
+                    print("MouseUp")
+                    self.select_cell(self.get_object_at(pygame.mouse.get_pos()))
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DELETE or event.key == pygame.K_BACKSPACE or event.key == pygame.K_0:
+                        self.set_number(0)
+                    elif event.key == pygame.K_1:
+                        self.set_number(1)
+                    elif event.key == pygame.K_2:
+                        self.set_number(2)
+                    elif event.key == pygame.K_3:
+                        self.set_number(3)
+                    elif event.key == pygame.K_4:
+                        self.set_number(4)
+                    elif event.key == pygame.K_5:
+                        self.set_number(5)
+                    elif event.key == pygame.K_6:
+                        self.set_number(6)
+                    elif event.key == pygame.K_7:
+                        self.set_number(7)
+                    elif event.key == pygame.K_8:
+                        self.set_number(8)
+                    elif event.key == pygame.K_9:
+                        self.set_number(9)
+            self.draw()
+
+class UI(object):
+    def __init__(self, sudoku):
+        self.screenSize = width, height = 600, 700
+        pygame.init()
+        font = pygame.font.SysFont('Tahoma', 60)
+        chars = [
+            font.render('1', True, (0, 0, 0)),
+            font.render('2', True, (0, 0, 0)),
+            font.render('3', True, (0, 0, 0)),
+            font.render('4', True, (0, 0, 0)),
+            font.render('5', True, (0, 0, 0)),
+            font.render('6', True, (0, 0, 0)),
+            font.render('7', True, (0, 0, 0)),
+            font.render('8', True, (0, 0, 0)),
+            font.render('9', True, (0, 0, 0))
+        ]
+
+        self.screen = pygame.display.set_mode(self.screenSize)
+    def draw(self, sudoku):
+        self.screen.fill((150, 150, 150))
+        for cell in sudoku.cells:
+            self.draw_cell(cell)
+        pygame.display.flip()
+
+    def draw_cell(self, cell):
+        square_size = self.screenSize[0] / 9
+        cell_x = square_size * cell.row
+        cell_y = square_size * cell.col
+        square = pygame.Rect(cell_x, cell_y, square_size, square_size)
+
+        pygame.draw.rect(self.screen, (255, 255, 255), square, width=2, border_radius=2)
+
+        if cell.number > 0:
+            self.screen.blit(self.chars[cell.number-1], (cell_x, cell_y))
 
 
 class Cell(object):
@@ -56,7 +117,15 @@ class Cell(object):
         self.col = col
         self.row = row
         self.number = 0
+        self.selected = False
 
+    def select(self):
+        self.selected = True
 
-game = Sudoku()
-game.print()
+    def deselect(self):
+        self.selected = False
+
+    def set_number(self, number):
+        self.number = number
+
+newGame = Sudoku()
