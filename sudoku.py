28bb76a5d6c6158cb6e37c7eeb5637d22831cd3c
UnfinishedProgram <noah.labrecque@gmail.com>
2021-01-08 15:56:33 -0500

    Attempted recursive solving algorithm

--- sudoku.py
+++ sudoku.py
@@ -1,30 +1,39 @@
 import math
-import sys
+
 import pygame
 import pygame.key
-
+import sys
+print(sys.getrecursionlimit())
 
 
 
 class Sudoku(object):
     def __init__(self):
-        self.ui = UI(self)
+        self.ui = UI()
         self.screen = None
-        self.regions = [[]] * 9
+        self.regions = [[] for rows in range(9)]
+        self.rows = [[] for rows in range(9)]
+        self.cols = [[] for rows in range(9)]
+
         self.cells = []
         self.selectedCell = None
 
         for i in range(9):
             for j in range(9):
-                self.cells.append(Cell(self, math.floor(i / 3) * 3 + math.floor(j / 3), i, j))
-                self.regions[math.floor(i / 3) * 3 + math.floor(j / 3)].append([self.cells[len(self.cells) - 1]])
+                region_index = math.floor(i / 3) * 3 + math.floor(j / 3)
+                self.cells.append(Cell(self, region_index, i, j))
+
+                self.regions[region_index].append(self.cells[len(self.cells) - 1])
+
+                self.rows[j].append(self.cells[len(self.cells) - 1])
+                self.cols[i].append(self.cells[len(self.cells) - 1])
         self.gameloop()
 
+
     def get_object_at(self, mouse):
-        if(mouse[1] < self.screenSize[0]):
-            square_x = math.floor(mouse[0] / (self.screenSize[0] / 9))
-            square_y = math.floor(mouse[1] / (self.screenSize[0] / 9))
-            print(square_x, square_y)
+        if mouse[1] < self.ui.screenSize[0]:
+            square_x = math.floor(mouse[0] / (self.ui.screenSize[0] / 9))
+            square_y = math.floor(mouse[1] / (self.ui.screenSize[0] / 9))
             return self.cells[square_y * 9 + square_x]
         else:
             pass
@@ -38,7 +47,8 @@ class Sudoku(object):
 
     def set_number(self, number):
         if self.selectedCell:
-            self.selectedCell.set_number(number)
+            if self.is_safe(self.selectedCell, number):
+                self.selectedCell.set_number(number)
 
     def gameloop(self):
         while 1:
@@ -72,14 +82,51 @@ class Sudoku(object):
                         self.set_number(8)
                     elif event.key == pygame.K_9:
                         self.set_number(9)
-            self.draw()
+                    elif event.key == pygame.K_SPACE:
+                        self.solve()
+            self.ui.draw(self)
 
+    def is_safe(self, cell, number):
+        if number == 0:
+            return True
+        for i in self.regions[cell.region]:
+            if number == cell.number:
+                return False
+
+        for i in self.rows[cell.row]:
+            if number == cell.number:
+                return False
+
+        for i in self.cols[cell.col]:
+            if number == cell.number:
+                return False
+        return True
+
+    def find_unassigned(self):
+        for cell in self.cells:
+            if cell.number == 0:
+                return cell
+        return False
+
+    def solve(self):
+        cell = self.find_unassigned()
+        if cell:
+            for num in range(9):
+                if self.is_safe(cell, num+1):
+                    cell.set_number(num+1)
+                    check = self.solve()
+                    if check:
+                        return True
+                    cell.set_number(0)
+            return False
+        else:
+            return True
 class UI(object):
-    def __init__(self, sudoku):
+    def __init__(self):
         self.screenSize = width, height = 600, 700
         pygame.init()
         font = pygame.font.SysFont('Tahoma', 60)
-        chars = [
+        self.chars = [
             font.render('1', True, (0, 0, 0)),
             font.render('2', True, (0, 0, 0)),
             font.render('3', True, (0, 0, 0)),
@@ -103,8 +150,11 @@ class UI(object):
         cell_x = square_size * cell.row
         cell_y = square_size * cell.col
         square = pygame.Rect(cell_x, cell_y, square_size, square_size)
+        if cell.selected:
+            pygame.draw.rect(self.screen, (255, 255, 255), square, border_radius=2)
+        else:
+            pygame.draw.rect(self.screen, (255, 255, 255), square, width=2, border_radius=2)
 
-        pygame.draw.rect(self.screen, (255, 255, 255), square, width=2, border_radius=2)
 
         if cell.number > 0:
             self.screen.blit(self.chars[cell.number-1], (cell_x, cell_y))
